1fcb2ed2c2cd13c76a485d41328fd10a69bad22a
User <User@DESKTOP-Q9VGQR4>
2023-05-31 22:42:50 +0530

    commit5

---
+++ LOGICAL/src/logical/Armstrongs.java
@@ -0,0 +1,32 @@
+package logical;
+
+import java.util.Scanner;
+
+public class Armstrongs {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter number");
+		int number=153;
+		int originalnum;
+		//int remainder;
+		int result=0;
+		originalnum=number;
+		
+		while(originalnum!=0)
+		{
+		int	remainder=originalnum%10;
+			result+=Math.pow(remainder, 3);
+		
+			originalnum/=10;
+		}
+		if(result==number)
+		{
+			System.out.println(number + " it is armstrong number");
+		}
+		else
+		{
+			System.out.println(number + " it is not armstrong number");
+		}
+		}
+}
