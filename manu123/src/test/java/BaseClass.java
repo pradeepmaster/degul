50b2972932cc2d5e708daec37b9fb795c9848032
pradeepmaster <pradeepachari58@gmail.com>
2023-06-02 17:43:56 +0530

    yes commit

--- manu123/src/test/java/BaseClass.java
+++ manu123/src/test/java/BaseClass.java
@@ -1,9 +1,7 @@
 
-package hrmGenericUtilities;
+
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import org.openqa.selenium.WebDriver;
@@ -16,34 +14,24 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
 
-import com.mysql.cj.jdbc.Driver;
-
-import POM.HomePage;
-import POM.LoginPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 public class BaseClass {
 
-	public	ExcelUtility eLib=new ExcelUtility();
-	public	FileUtility fLib=new FileUtility();
-	public	JavaUtility jLib=new JavaUtility();
-	public	WebDriverUtility wLib=new WebDriverUtility();
-	public DataBaseUtility dLib=new DataBaseUtility();
+	
 	public WebDriver driver;
-	public static WebDriver lDriver;
 	
 //		connect to DB
-		@BeforeSuite(groups= {"smokeTest","regressionTest"})
+		@BeforeSuite()
 		public void connectDB() throws SQLException {
 			//dLib.connectToDB();
 			System.out.println("-----Connected to DB-----");
 		}
 		
 //		close the DB connection
-		@AfterSuite(groups= {"smokeTest","regressionTest"})
+		@AfterSuite()
 		public void closeBD() throws SQLException {
 			//dLib.closeDB();
 			System.out.println("-----Closed to DB-----");
@@ -51,7 +39,7 @@ public class BaseClass {
 		
 //		Launch the Browser
 //		@Parameters("browser")
-		@BeforeClass(groups= {"smokeTest","regressionTest"})
+		@BeforeClass()
 		public void BC() throws IOException {
 			//String browser=fLib.getPropertyFileKeyValue("browser");
 			
@@ -71,35 +59,21 @@ public class BaseClass {
 		}
 		
 //		close the browser
-		@AfterClass(groups= {"smokeTest","regressionTest"})
+		@AfterClass()
 		public void AC() {
 			driver.quit();
 			System.out.println("-----Closed the Browser-----");
 		}
 		
 //		Logging to the application as HR Head
-		@BeforeMethod(groups= {"smokeTest","regressionTest"})
+		@BeforeMethod()
 		public void BM() throws IOException {
-			String url=fLib.getPropertyFileKeyValue("url");
-			String headUsername=fLib.getPropertyFileKeyValue("headUsername");
-			String headPassword=fLib.getPropertyFileKeyValue("headPassword");
-			String hrHeadVisibleTxt=fLib.getPropertyFileKeyValue("hrHeadVisibleTxt");
-			driver.get(url);
-			lDriver=driver;
-//			maximize the window
-			wLib.maximizeWindow(driver);
-//			implicitly wait for period of 10seconds
-			wLib.waitTillPageGetsLoad(driver);
-			LoginPage loginPage=new LoginPage(driver);
-			loginPage.loginToApp(headUsername, headPassword, hrHeadVisibleTxt);
 			System.out.println("-----Logging to App is completed-----");
 		}
 		
 //		logout from the Application
-		@AfterMethod(groups= {"smokeTest","regressionTest"})
+		@AfterMethod()
 		public void AM() {
-			HomePage homePage=new HomePage(driver);
-			homePage.logOutFromApp();
 //			wLib.switchToAlertPopupAcceptAndPrintTxt(driver);
 			System.out.println("-----LogOut from App is completed-----");
 		}
